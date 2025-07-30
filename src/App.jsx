8205b5aef6a566398ec2e3c852c0c8dedac9de18
Thiago Ferreira <thiagoferreira0231@gmail.com>
2025-07-30 11:21:53 -0300

    Iniciando a criação da Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './style/App.scss';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+          <Hero/>
+      </main>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <>
+            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, beatae.</h2>
+        </>
+    );
+};
+
+export default Hero;
